ebb7019ea78a32c230532688f0049ca2017b48c4
malekneji417-sudo <malekneji417@gmail.com>
2026-02-03 23:37:42 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const yesBtn = document.getElementById("yes");
+const noBtn = document.getElementById("no");
+
+// NO che scappa
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
+  const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
+
+  noBtn.style.position = "absolute"
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+});
+
+// YES che cresce
+yesBtn.addEventListener("click", () => {
+  let size = parseFloat(window.getComputedStyle(yesBtn).fontSize);
+  yesBtn.style.fontSize = (size + 6) + “px”;
+
+  if (size > 80) {
+    alert(“OKAY OKAY 😭💖 Sei il mio Valentino!”);
+  }
+});
